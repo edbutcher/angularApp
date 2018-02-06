0ac4316a15bf1eae05c8f81a9e64bf1b8bf37b9c
Ed Butcher <edbutcher200@gmail.com>
2018-02-06 23:02:36 +0200

    about page. Add bootstrap4 to the index.html

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { AboutComponent } from '../about/about.component';
+import { CartComponent } from '../cart/cart.component';
+import { HomeComponent } from '../home/home.component';
+import { ProductsComponent } from '../products/products.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  // { path: 'products', component: AboutComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent }
 ];
